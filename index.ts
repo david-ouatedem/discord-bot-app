fbcc1566dcfc66a6acbc7eb080f86ab322d54215
david-ouatedem <davdev1400@gmail.com>
2024-06-16 10:22:06 +0100

    added scripts

--- index.ts
+++ index.ts
@@ -1,19 +1,13 @@
-// Require the necessary discord.js classes
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 const cron = require('node-cron');
-// Load environment variables from .env file
 import 'dotenv/config'
 
 const token = process.env.DISCORD_TOKEN
 
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const generalChannelId = process.env.GENRAL_CHANNEL_ID
-const cronExpression = '* * * *' // 0 15 * * 1,2 , * * * *
+const cronExpression = '0 15 * * 1,2' // 0 15 * * 1,2 , * * * * *
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
     cron.schedule(cronExpression, () => {
@@ -28,5 +22,4 @@ client.once(Events.ClientReady, readyClient => {
 });
 
 
-// Log in to Discord with your client's token
 client.login(token);
