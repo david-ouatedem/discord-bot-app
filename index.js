e0a3ff1ce9c0d98d5f09588843c688b33ff02f34
david-ouatedem <davdev1400@gmail.com>
2024-07-15 20:26:47 +0100

    changed message

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
-import express from 'express'
-import cron from 'node-cron';
-import 'dotenv/config'
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import express from "express";
+import cron from "node-cron";
+import "dotenv/config";
 
-const app = express()
-const token = process.env.DISCORD_TOKEN
-const port = process.env.PORT
+const app = express();
+const token = process.env.DISCORD_TOKEN;
+const port = process.env.PORT;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const generalChannelId = process.env.GENRAL_CHANNEL_ID
-const cronExpression = '0 15 * * 1,2' // 0 15 * * 1,2 , * * * * *
+const generalChannelId = process.env.GENRAL_CHANNEL_ID;
+const cronExpression = "0 15 * * 1,2"; // 0 15 * * 1,2 , * * * * *
 
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-    cron.schedule(cronExpression, () => {
-        const channel = client.channels.cache.get(generalChannelId??'');
-        if (channel && channel.isTextBased() ) {
-            channel.send('This is your scheduled message.');
-        } else {
-            console.error('Channel not found');
-        }
-    });
-
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+  cron.schedule(cronExpression, () => {
+    const channel = client.channels.cache.get(generalChannelId ?? "");
+    if (channel && channel.isTextBased()) {
+      channel.send("Hey Dave dont forget to send your weekly objectives");
+    } else {
+      console.error("Channel not found");
+    }
+  });
 });
-
 
 client.login(token);
 
-
-app.get('/', (req, res) => {
-    res.send('Discord bot is running!');
+app.get("/", (req, res) => {
+  res.send("Discord bot is running!");
 });
 
 app.listen(port, () => {
-    console.log(`Express server is listening on port ${port}`);
+  console.log(`Express server is listening on port ${port}`);
 });
